12bc9902c71bc03948759be0c91fb7c88aa66a67
Pengularity <88403571+Pengularity@users.noreply.github.com>
2023-09-26 23:18:30 +0200

    Add files via upload

---
+++ init.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pengu <pengu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 20:46:20 by pengu             #+#    #+#             */
+/*   Updated: 2023/09/26 22:17:59 by pengu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philosophers.h"
+
+void init_philo_and_forks(t_philosopher *philo, pthread_mutex_t *forks, int philo_nb)
+{
+    int i;
+
+    i = 0;
+    while (i < philo_nb)
+    {
+        pthread_mutex_init(&forks[i], NULL);
+        philo[i].id = i + 1;
+        philo[i].left_fork = &forks[i];
+        philo[i].right_fork = &forks[(i + 1) % philo_nb];
+        gettimeofday(&philo[i].last_time_ate, NULL);
+        philo[i].times_eaten = 0;
+        i++;
+    }
+}
