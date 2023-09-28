30657a81ad4200fa9569397d48622ad4ec71c734
wnguyen <pengularity@gmail.com>
2023-09-28 18:43:10 +0200

    ongoing

---
+++ include/philosophers.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wnguyen <wnguyen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 20:37:52 by pengu             #+#    #+#             */
+/*   Updated: 2023/09/28 18:37:03 by wnguyen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef enum e_simulation_status
+{
+	ONGOING,
+	PHILOSOPHER_DIED,
+	ALL_ATE_REQUIRED_TIMES
+}				t_simulation_status;
+
+typedef struct s_config
+{
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_must_eat;
+}				t_config;
+
+typedef struct s_simulation
+{
+	pthread_mutex_t		*forks;
+	int					philo_nb;
+	t_config			*config;
+	t_simulation_status	status;
+	t_philosopher		*philosophers;
+}					t_simulation;
+
+typedef struct s_philosopher
+{
+	int				id;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	struct timeval	last_time_ate;
+	int				times_eaten;
+	t_simulation	*simulation;
+}						t_philosopher;
+
+void		*philosopher_routine(void *arg);
+long int	current_time(void);
+
+#endif
---
+++ src/routine.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wnguyen <wnguyen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 21:25:06 by pengu             #+#    #+#             */
+/*   Updated: 2023/09/28 18:40:22 by wnguyen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philosophers.h"
+
+void	print_status(t_philosopher *philo, char *status)
+{
+	printf("%ld %d %s\n", current_time, philo->id, status);
+}
+
+void	think(t_philosopher *philo)
+{
+	print_status(philo, "is thinking");
+	// Implementation delay thinking.
+}
+
+void	eat(t_philosopher *philo)
+{
+	t_simulation	*simulation;
+
+	simulation = philo->simulation;
+	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
+	print_status(philo, "has taken a fork");
+	print_status(philo, "is eating");
+	gettimeofday(&philo->last_time_ate, NULL);
+	ft_sleep(simulation->config->time_to_eat);
+	philo->times_eaten++;
+	if (simulation->config->nb_must_eat > 0
+		&& philo->times_eaten >= simulation->config->nb_must_eat)
+		simulation->status = ALL_ATE_REQUIRED_TIMES;
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
+
+void	sleep(t_philosopher *philo)
+{
+	t_simulation	*simulation;
+
+	simulation = philo->simulation;
+	print_status(philo, "is sleeping");
+	ft_sleep(simulation->config->time_to_sleep);
+}
+
+void	*philosopher_routine(void *arg)
+{
+	t_philosopher	*philo;
+	t_simulation	*simulation;
+
+	philo = (t_philosopher *)arg;
+	simulation = philo->simulation;
+	while (simulation->status == ONGOING)
+	{
+		think(philo);
+		eat(philo);
+		sleep(philo);
+	}
+	return (NULL);
+}
